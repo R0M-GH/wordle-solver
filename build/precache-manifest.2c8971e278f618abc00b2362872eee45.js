f2be3db321b86f009ca8f01afef3efc6b2fc4abe
R0M-GH <rom.malla@outlook.com>
2022-10-23 00:24:53 -0700

    Final App Commit

---
+++ build/precache-manifest.2c8971e278f618abc00b2362872eee45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4a480027f709099c790",
+    "url": "/static/css/main.15a32eeb.chunk.css"
+  },
+  {
+    "revision": "b4a480027f709099c790",
+    "url": "/static/js/main.b4a48002.chunk.js"
+  },
+  {
+    "revision": "bf1c5ade5df97ad1183b",
+    "url": "/static/js/1.bf1c5ade.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3381988a0dfdab4e2edb1ca79216c9c7",
+    "url": "/static/media/words.3381988a.txt"
+  },
+  {
+    "revision": "202cd961977c1b760217e9419dc3b380",
+    "url": "/index.html"
+  }
+];
